2e9de1ce046ab3e14fd124d049f4950a2d4f3a6a
FreddyMSchubert <github@frederickschubert.de>
2025-08-09 21:00:00 +0000

    improved server config & default gridmaster bot

--- gridmaster/src/bot.h
+++ gridmaster/src/bot.h
@@ -3,8 +3,6 @@
 
 #include "core_lib.h"
 
-void move_unit_to(t_obj *unit, t_pos target);
-
 t_obj *ft_get_core_own(void);
 t_obj *ft_get_core_opponent(void);
 t_obj *ft_get_resource_nearest(t_pos pos);
