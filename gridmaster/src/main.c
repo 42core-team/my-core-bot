d88b32481441d30781202b8c58809d41de184d91
FreddyMSchubert <github@frederickschubert.de>
2025-08-27 06:40:45 +0200

    bot adjustments to adhere to new changes as per https://github.com/42core-team/monorepo/issues/37

--- gridmaster/src/main.c
+++ gridmaster/src/main.c
@@ -5,7 +5,7 @@
 
 void ft_on_tick(unsigned long tick);
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	return core_startGame("Gridmaster", argc, argv, ft_on_tick, false);
 }
@@ -15,7 +15,7 @@ void move_and_attack(t_obj *unit, t_pos target_pos)
 	int dx = target_pos.x - unit->pos.x;
 	int dy = target_pos.y - unit->pos.y;
 
-	t_pos next_pos = { unit->pos.x, unit->pos.y };
+	t_pos next_pos = {unit->pos.x, unit->pos.y};
 	if (abs(dx) > abs(dy))
 	{
 		int step = (dx > 0) ? 1 : -1;
@@ -29,16 +29,9 @@ void move_and_attack(t_obj *unit, t_pos target_pos)
 
 	t_obj *next_pos_obj = core_get_obj_from_pos(next_pos);
 	if (next_pos_obj)
-	{
-		if (next_pos_obj->type == OBJ_GEM_PILE)
-			core_action_move(unit, next_pos);
-		else
-			core_action_attack(unit, next_pos);
-	}
+		core_action_attack(unit, next_pos);
 	else
-	{
 		core_action_move(unit, next_pos);
-	}
 }
 
 int target_unit = 1;
@@ -66,24 +59,24 @@ void ft_on_tick(unsigned long tick)
 
 		switch ((int)obj->s_unit.unit_type)
 		{
-			case UNIT_WARRIOR:
-				t_obj *closest_opponent = ft_get_units_opponent_nearest(ft_get_core_own()->pos);
-				if (closest_opponent)
-					move_and_attack(obj, closest_opponent->pos);
-				else
-					move_and_attack(obj, ft_get_core_opponent()->pos);
-				break;
+		case UNIT_WARRIOR:
+			t_obj *closest_opponent = ft_get_units_opponent_nearest(ft_get_core_own()->pos);
+			if (closest_opponent)
+				move_and_attack(obj, closest_opponent->pos);
+			else
+				move_and_attack(obj, ft_get_core_opponent()->pos);
+			break;
 
-			case UNIT_MINER:
-				t_obj *nearest_resource_or_money = ft_get_resource_money_nearest(obj->pos);
-				if (nearest_resource_or_money && obj->s_unit.gems <= 0)
-					move_and_attack(obj, nearest_resource_or_money->pos);
-				else
-				{
-					move_and_attack(obj, ft_get_core_own()->pos);
-					core_action_transferGems(obj, ft_get_core_own()->pos, 9999999);
-				}
-				break;
+		case UNIT_MINER:
+			t_obj *nearest_resource_or_money = ft_get_resource_money_nearest(obj->pos);
+			if (nearest_resource_or_money && obj->s_unit.gems <= 0)
+				move_and_attack(obj, nearest_resource_or_money->pos);
+			else
+			{
+				move_and_attack(obj, ft_get_core_own()->pos);
+				core_action_transferGems(obj, ft_get_core_own()->pos, 9999999);
+			}
+			break;
 		}
 	}
 	free(units);
--- my-core-bot/src/pathfinding.c
+++ my-core-bot/src/pathfinding.c
@@ -2,8 +2,10 @@
 
 static int ft_sign(int num)
 {
-	if (num > 0) return 1;
-	if (num < 0) return -1;
+	if (num > 0)
+		return 1;
+	if (num < 0)
+		return -1;
 	return 0;
 }
 
@@ -21,20 +23,20 @@ static bool ft_is_own_obj(t_obj *obj)
 static bool ft_is_pos_blocked(t_pos pos)
 {
 	t_obj *obj = core_get_obj_from_pos(pos);
-	if (obj && obj->type != OBJ_GEM_PILE)
+	if (obj)
 		return true;
 	if (pos.x >= game.config.gridSize || pos.y >= game.config.gridSize)
 		return true;
 	return false;
 }
 
-void	ft_pathfind(t_obj *moving_unit, t_pos target_pos)
+void ft_pathfind(t_obj *moving_unit, t_pos target_pos)
 {
 	int dx = target_pos.x - moving_unit->pos.x;
 	int dy = target_pos.y - moving_unit->pos.y;
 
-	t_pos nextPosOptionHorizontal = { moving_unit->pos.x + ft_sign(dx), moving_unit->pos.y };
-	t_pos nextPosOptionVertical = { moving_unit->pos.x, moving_unit->pos.y + ft_sign(dy) };
+	t_pos nextPosOptionHorizontal = {moving_unit->pos.x + ft_sign(dx), moving_unit->pos.y};
+	t_pos nextPosOptionVertical = {moving_unit->pos.x, moving_unit->pos.y + ft_sign(dy)};
 
 	t_obj *nextPosOptionHorizontalObj = core_get_obj_from_pos(nextPosOptionHorizontal);
 	t_obj *nextPosOptionVerticalObj = core_get_obj_from_pos(nextPosOptionVertical);
